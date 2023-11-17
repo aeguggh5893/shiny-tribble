1e10f7a4b365e1ec34b409fd8159160ec4c36a95
rich <dankwarrior6900@gmail.com>
2023-11-17 14:50:54 -0800

    Finally made the css stylesheet load utilzing the loader feature

--- src/homepage.js
+++ src/homepage.js
@@ -5,6 +5,7 @@ image.setAttribute("alt", "naruto-banner");
 
 const h1 = document.createElement('h1');
 h1.textContent = "Naruto Next Generations!";
+h1.classList.add('hello');
 
 const p = document.createElement("p");
 p.textContent = "Relive the greatest moments again from one of the most beloved and popular series from the 2000's!!!";
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-"use strict";
-
 import {image, h1, p} from './homepage.js';
+import './style.css';
 
 let content = document.querySelector('#content');
-
 content.appendChild(image);
 content.appendChild(h1);
 content.appendChild(p);
+
+let h2 = document.createElement('h2');
+h2.textContent = 'This should be red';
+h2.classList.add('red');
+content.appendChild(h2);
